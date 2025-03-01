b158a93db0cf64568a9f2595ff5bb3d391749a28
Luis Alberto <m1@MacBookPro.lan>
2025-03-01 16:44:01 -0500

    Initial commit - set up React app with TypeScript

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <header style={{ width: "100%", display: "flex", justifyContent: "center", alignItems: "center", padding: "10px" }}>
+      <h1>Tech Equipment Registry</h1>
+    </header>
+  );
+};
+
+export default Header;
